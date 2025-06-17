5da14072a576a217c594228f726c83bf61cacd69
백종혁 <jonghyuck.baek@togle.shop>
2025-06-17 20:04:30 +0900

    새로고침, url 직접 입력시 404 페이지 오류 관련 수정(github pages SPA 사이트 url 미지원)

--- assets/IndexPage.js
+++ assets/IndexPage.js
@@ -1,11 +1,14 @@
 import { Q as QPage } from "./QPage.js";
-import { s as defineComponent, y as openBlock, z as createBlock } from "./index.js";
+import { s as defineComponent, v as onMounted, y as openBlock, z as createBlock } from "./index.js";
 const _sfc_main = defineComponent({
   ...{
     name: "IndexPage"
   },
   __name: "IndexPage",
   setup(__props) {
+    onMounted(() => {
+      console.log(window.location);
+    });
     return (_ctx, _cache) => {
       return openBlock(), createBlock(QPage, { class: "t-page" });
     };
